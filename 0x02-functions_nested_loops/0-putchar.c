ccc0d39bcc823dfc7eb7974f0b21bc8a2d48b031
Jamil Mohammed <zeus2tee@gmail.com>
2023-02-21 03:37:26 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+/*
+ * A program that prints _putchar, followed by a new line.
+ *
+ * The program should return 0
+ */
+
+#include <stdio.h>
+
+/**
+ * main - program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *sh = "ALX";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
